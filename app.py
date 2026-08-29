e1a1b411e362b3239d413df2abafddfe16d22962
nataliaSilva <nataliapaula695@gmail.com>
2026-08-29 20:45:35 -0300

    Ativ em Python
    Atv em python, resolução de problemas simples

---
+++ app.py
@@ -0,0 +1,16 @@
+#Crie um programa que solicite ao usuario:
+# Nome;
+# Idade;
+#Cidade onde mora.
+#Ao final, exiba uma mensagem, por exemplo:
+#Ola, Leonardo! Voce tem 25 anos e mora em Londrina.
+
+print("\nOlah, este programa irah lhe mostrar seu nome, idade e a cidade ao qual voceh mora")
+
+nome = input("\nQual e o seu nome? ")
+idade = input("\nQual a sua idade? ")
+cidade = input("\nQual a sua cidade? ")
+
+print("\nSeu nome eh: ", nome)
+print("\nSua idade eh: ", idade)
+print("\nSua cidade eh: ", cidade)
