94af23d44266eb2e4fc35fba68d9a45540cbc9ac
nataliaSilva <nataliapaula695@gmail.com>
2026-08-29 20:04:34 -0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,8 @@
+#Crie um programa que solicite ao usuario:
+# Nome;
+# Idade;
+#Cidade onde mora.
+#Ao final, exiba uma mensagem, por exemplo:
+#Ola, Leonardo! Voce tem 25 anos e mora em Londrina.
+
+python
