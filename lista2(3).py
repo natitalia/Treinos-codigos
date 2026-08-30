f7ac9119f8282d06fa3bbd6f38ec538f6bda95ba
nataliaSilva <nataliapaula695@gmail.com>
2026-08-30 01:00:01 -0300

    Números usando for e range()

---
+++ lista2(3).py
@@ -0,0 +1,7 @@
+#3. Exercício 3 – Números de 1 até 20
+#Utilizando for e range(), mostre todos os números de 1 até 20.
+
+print("\nEste programa usa for para contar de 1 até 20")
+for num in range(1, 21):
+    print(num)
+print("Acabooooooou! :D")
