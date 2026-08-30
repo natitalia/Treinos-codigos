a685548b287e03bc1a48fd38a5de87ab33a3a3a8
nataliaSilva <nataliapaula695@gmail.com>
2026-08-29 22:14:04 -0300

    Lembrete p/ mim no futuro
    input é string, ou seja, só para textos, então para este caso, terei de converter as letras(números escolhidos) para usar int ou float (inteiro ou quebrado)

--- app2.py
+++ app2.py
@@ -5,4 +5,20 @@
 #• Multiplicação;
 #• Divisão.
 
+print("\nOlá, este programa é uma calculadora! Teste e comprove :D")
 
+a = input("\nDigite um número: ")
+b = input("Digite outro número: ")
+
+a = int(a)
+b = int(b)
+
+soma = a + b
+subt = a - b
+mult = a * b
+div = a / b
+
+print("\nSoma: ",soma)
+print("Subtração: ",subt)
+print("Multiplicação: ",mult)
+print("Divisão: ",div)
