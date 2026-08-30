05723ecda4e7bf8ff4d7c2521d06af5ebc7a662d
nataliaSilva <nataliapaula695@gmail.com>
2026-08-30 00:42:03 -0300

    Contagem de 1 até 10
    Usando while

---
+++ lista2(1).py
@@ -0,0 +1,11 @@
+#1. Exercício 1 – Contagem de 1 até 10
+#Utilizando while, mostre na tela os números de 1 até 10
+
+print("\nEste programa conta de 1 até 10")
+
+num = 1
+
+while num <= 10:
+    print(num)
+    num = num + 1
+print("\nAcabooooooooooou")
--- python.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que solicite ao usuario:
-# Nome;
-# Idade;
-#Cidade onde mora.
-#Ao final, exiba uma mensagem, por exemplo:
-#Ola, Leonardo! Voce tem 25 anos e mora em Londrina.
-
-python
