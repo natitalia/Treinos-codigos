ad6fb0c1e80cc95af59d587e3e3ac45e63678e0a
nataliaSilva <nataliapaula695@gmail.com>
2026-08-30 01:10:18 -0300

    contando os números pares de 0 a 20
    Usando for e range()
    0 -> início
    21 -> fim
    2 -> passo

---
+++ lista2(4).py
@@ -0,0 +1,8 @@
+#4. Exercício 4 – Números pares
+#Utilizando for e range(), mostre todos os números pares entre 0 e 20.
+
+print("\nEste programa mostra todos os números pares de 0 até 20!")
+
+for num in range(0, 21, 2):
+    print(num)
+print("Acabooooooou :D")
