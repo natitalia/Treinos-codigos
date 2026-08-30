531b81c9a8d2bf0fd124a082665fdc1a7e6498e1
nataliaSilva <nataliapaula695@gmail.com>
2026-08-29 22:30:48 -0300

    Calculadora de idade
    Programa para ver quantos anos você terá em 2026

---
+++ app3.py
@@ -0,0 +1,15 @@
+#3. Exercício 3 – Calculando a idade
+#Crie um programa que solicite o nome e o ano de nascimento de uma pessoa. Em
+#seguida, calcule a idade que ela completa em 2026 e exiba:
+#Maria completa 30 anos em 2026.
+
+print("\nOlá, este programa é uma calculadora de idade, vamos descobrir quantos anos você fará em...")
+print("...2026!")
+
+nasc = input("\nDigite o ano do seu nascimento: ")
+nasc = int(nasc)
+soma2026 = 2026 - nasc 
+
+print("\nVocê terá:",soma2026,"em 2026!")
+
+
