a6f97319052e24780cb8cf9c7054b6ecefc9d0c8
nataliaSilva <nataliapaula695@gmail.com>
2026-08-30 00:49:20 -0300

    Contagem regressiva
    Mesmo da anterior, porém ao contrário

---
+++ lista2(2).py
@@ -0,0 +1,14 @@
+#2. Exercício 2 – Contagem regressiva
+#Utilizando while, mostre os números:
+#10 9 8 7 6 5 4 3 2 1
+#Depois exiba:
+#Fim!
+
+print("Este programa conta de 10 até 1")
+
+num = 10
+
+while num >=1:
+    print(num)
+    num = num - 1
+print("\nFim!")
