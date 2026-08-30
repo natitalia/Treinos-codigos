63203c77efb0a65d1bb869babf3dddceba94b65c
nataliaSilva <nataliapaula695@gmail.com>
2026-08-29 21:35:10 -0300

    Calculadora simples
    Criação de uma calculadora simples: soma, subtração, multiplicação e divisão

---
+++ app2.py
@@ -0,0 +1,8 @@
+#2. Exercício 2 – Calculadora simples
+#Crie um programa que solicite dois números inteiros ao usuário. Em seguida, mostre:
+#• Soma;
+#• Subtração;
+#• Multiplicação;
+#• Divisão.
+
+
